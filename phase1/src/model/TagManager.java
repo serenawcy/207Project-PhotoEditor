223b981bd15c1d5f0d77d7495b8f7064d341b349
Chenyue Wang <serenachenyue.wang@mail.utoronto.ca>
2017-11-15 22:16:32 -0500

    Made the class TagManager implement Serializable so that we can save old tags

--- phase1/src/model/TagManager.java
+++ phase1/src/model/TagManager.java
@@ -1,9 +1,10 @@
 package model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class TagManager {
+public class TagManager implements Serializable {
     private ArrayList<String> allTags;
 
-    
+
 }

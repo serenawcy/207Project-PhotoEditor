4ac9e07fea8671c5b20043e2864798cf39ace77d
Chenyue Wang <serenachenyue.wang@mail.utoronto.ca>
2017-11-16 16:29:11 -0500

    Added a method checkExist in class ImageFileManager to check whether the chosen file exists, and then return this ImageFile if exists or return a new one otherwise

--- phase1/src/model/ImageFileManager.java
+++ phase1/src/model/ImageFileManager.java
@@ -1,6 +1,8 @@
 package model;
 
 
+//import java.io.IOException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ImageFileManager {
@@ -8,5 +10,17 @@ public class ImageFileManager {
 
     public ImageFileManager(){
         this.imageList = new ArrayList<ImageFile>();
+    }
+
+    public ImageFile checkExist(String name, String absoluteAddress) throws IOException {
+        //if the image exists according to its absoluteAddress, return this image
+        for (ImageFile checkImage: this.imageList) {
+            if (checkImage.getAbsoluteAddress().equals(absoluteAddress)) {
+                return checkImage;
+            }
+        }
+        ImageFile newImageFile = new ImageFile(name, absoluteAddress);
+        this.imageList.add(newImageFile);
+        return newImageFile;
     }
 }

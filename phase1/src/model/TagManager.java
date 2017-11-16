978549c87820d7eaed1a06b1fbadb25d6271dc79
Chenyue Wang <serenachenyue.wang@mail.utoronto.ca>
2017-11-16 01:46:36 -0500

    Added an attribute filePath and constructed a constructor for TagManager.

--- phase1/src/model/TagManager.java
+++ phase1/src/model/TagManager.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 
 public class TagManager implements Serializable {
     private ArrayList<String> allTags;
+    private String filePath;
+
+    public TagManager(String imageID) {
+        this.filePath = imageID;
+    }
 
 
 }

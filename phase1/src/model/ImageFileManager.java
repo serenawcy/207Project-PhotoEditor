97816bf076f307e7bddc2bb2ffcd3d0453cff542
Chenyue Wang <serenachenyue.wang@mail.utoronto.ca>
2017-11-16 16:16:38 -0500

    Added a constructor for ImageFileManager

--- phase1/src/model/ImageFileManager.java
+++ phase1/src/model/ImageFileManager.java
@@ -6,5 +6,7 @@ import java.util.ArrayList;
 public class ImageFileManager {
     private ArrayList<ImageFile> imageList;
 
-    
+    public ImageFileManager(){
+        this.imageList = new ArrayList<ImageFile>();
+    }
 }

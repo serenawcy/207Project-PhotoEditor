031781b6b9758d677979e166777a27728071af4d
“Yuhan <yuhan.shao@mail.utoronto.ca>
2017-11-15 21:20:26 -0500

    Add instance variables, constructor, getters and setters to Image class

--- phase1/src/model/Image.java
+++ phase1/src/model/Image.java
@@ -24,84 +24,84 @@ public class Image {
         // TODO: Log and Handler
     }
 
-    /**
-     * Add user input tag name if it does not exist and rename this Image.
-     * @param userInputAdd the tag name which is supposed to be added to this Image name
-     */
-    public void addTag(String userInputAdd) {
-        String[]  nameToAdd = userInputAdd.split(",");
-        for (String name: nameToAdd) {
-            if (!this.tagStore.checkExist(name)) {
-                this.tagStore.addTag(name);
-                this.renameAdd(name);
-            }
-        }
-
-    }
-
-    /**
-     * Delete user input tag name if it exists and rename this Image.
-     * @param userInputDelete the tag name which is supposed to be deleted from this Image name
-     */
-    public void deleteTag(String userInputDelete) {
-        String[]  nameToDelete = userInputDelete.split(",");
-        for (String name: nameToDelete) {
-            if (!this.tagStore.checkExist(name)) {
-                this.tagStore.deleteTag(name);
-                this.renameDelete(name);
-            }
-        }
-    }
-
-    /**
-     * Change the absolute directory of this Image object.
-     * @param newImageName new name of this Image object
-     */
-    public void changeDirectory(String newImageName) {
-        this.absoluteAddress = this.absoluteAddress.replace(this.name, newImageName);
-    }
-
-    /**
-     * Change the name of this Image object.
-     * @param newImageName new name of this Image object
-     */
-    public void changeImageName(String newImageName) {
-        this.name = newImageName;
-        this.changeDirectory(newImageName);
-        // TODO: Log
-    }
-
-    /**
-     * Rename this Image by adding a tag.
-     * @param tagToAdd the tag name need to add
-     */
-    public void renameAdd(String tagToAdd) {
-        this.changeImageName(this.name + "@" + tagToAdd);
-    }
-
-    /**
-     * Rename this Image by deleting a tag.
-     * @param tagToDelete the tag name need to delete
-     */
-    public void renameDelete(String tagToDelete) {
-        this.changeImageName(this.name.replace("@" + tagToDelete, ""));
-    }
-
-    /**
-     * Get this Image object's tagStore.
-     * @return a TagManager object
-     */
-    public TagManager getTagManager() {
-        return this.tagStore;
-    }
-
-    /**
-     * Get the log history of this Image object.
-     * @return the String of log history
-     */
-    public String getLog() {
-        // TODO: Log
-    }
+//    /**
+//     * Add user input tag name if it does not exist and rename this Image.
+//     * @param userInputAdd the tag name which is supposed to be added to this Image name
+//     */
+//    public void addTag(String userInputAdd) {
+//        String[]  nameToAdd = userInputAdd.split(",");
+//        for (String name: nameToAdd) {
+//            if (!this.tagStore.checkExist(name)) {
+//                this.tagStore.addTag(name);
+//                this.renameAdd(name);
+//            }
+//        }
+//
+//    }
+//
+//    /**
+//     * Delete user input tag name if it exists and rename this Image.
+//     * @param userInputDelete the tag name which is supposed to be deleted from this Image name
+//     */
+//    public void deleteTag(String userInputDelete) {
+//        String[]  nameToDelete = userInputDelete.split(",");
+//        for (String name: nameToDelete) {
+//            if (!this.tagStore.checkExist(name)) {
+//                this.tagStore.deleteTag(name);
+//                this.renameDelete(name);
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Change the absolute directory of this Image object.
+//     * @param newImageName new name of this Image object
+//     */
+//    public void changeDirectory(String newImageName) {
+//        this.absoluteAddress = this.absoluteAddress.replace(this.name, newImageName);
+//    }
+//
+//    /**
+//     * Change the name of this Image object.
+//     * @param newImageName new name of this Image object
+//     */
+//    public void changeImageName(String newImageName) {
+//        this.name = newImageName;
+//        this.changeDirectory(newImageName);
+//        // TODO: Log
+//    }
+//
+//    /**
+//     * Rename this Image by adding a tag.
+//     * @param tagToAdd the tag name need to add
+//     */
+//    public void renameAdd(String tagToAdd) {
+//        this.changeImageName(this.name + "@" + tagToAdd);
+//    }
+//
+//    /**
+//     * Rename this Image by deleting a tag.
+//     * @param tagToDelete the tag name need to delete
+//     */
+//    public void renameDelete(String tagToDelete) {
+//        this.changeImageName(this.name.replace("@" + tagToDelete, ""));
+//    }
+//
+//    /**
+//     * Get this Image object's tagStore.
+//     * @return a TagManager object
+//     */
+//    public TagManager getTagManager() {
+//        return this.tagStore;
+//    }
+//
+//    /**
+//     * Get the log history of this Image object.
+//     * @return the String of log history
+//     */
+//    public String getLog() {
+//        // TODO: Log
+//    }
 
     /**
      * Get this Image object's name.

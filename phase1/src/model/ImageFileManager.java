502fccdbc35e91b1b379df4cb9d077c99ddd2b09
Chenyue Wang <serenachenyue.wang@mail.utoronto.ca>
2017-11-16 23:54:31 -0500

    Made the attribute imageList to be static so that all instances of ImageFileManager share one list to keep track opened files

--- phase1/src/model/ImageFileManager.java
+++ phase1/src/model/ImageFileManager.java
@@ -6,21 +6,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ImageFileManager {
-    private ArrayList<ImageFile> imageList;
+    private static ArrayList<ImageFile> imageList = new ArrayList<ImageFile>();
 
     public ImageFileManager(){
-        this.imageList = new ArrayList<ImageFile>();
     }
 
     public ImageFile checkExist(String name, String absoluteAddress) throws IOException {
         //if the image exists according to its absoluteAddress, return this image
-        for (ImageFile checkImage: this.imageList) {
+        for (ImageFile checkImage: imageList) {
             if (checkImage.getAbsoluteAddress().equals(absoluteAddress)) {
                 return checkImage;
             }
         }
+        // if not, return a new ImageFile
         ImageFile newImageFile = new ImageFile(name, absoluteAddress);
-        this.imageList.add(newImageFile);
+        imageList.add(newImageFile);
         return newImageFile;
     }
 }

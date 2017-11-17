5633e0f451fc66e1cb20a60703788031d3304f3c
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2017-11-16 20:48:26 -0500

    Delete unused import

--- phase1/src/model/ImageFile.java
+++ phase1/src/model/ImageFile.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
-import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 

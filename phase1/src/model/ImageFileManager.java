55053d341ee3d55fc3e7dc94e048ba53d6e7422e
Chenyue Wang <serenachenyue.wang@mail.utoronto.ca>
2017-11-17 19:12:11 -0500

    Changed unmeaningful attribute name in class ImageFileManager from imageList to imageFileList

--- phase1/src/model/ImageFileManager.java
+++ phase1/src/model/ImageFileManager.java
@@ -6,21 +6,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ImageFileManager {
-    private static ArrayList<ImageFile> imageList = new ArrayList<ImageFile>();
+    private static ArrayList<ImageFile> imageFileList = new ArrayList<ImageFile>();
 
     public ImageFileManager(){
     }
 
     public ImageFile checkExist(String name, String absoluteAddress) throws IOException {
         //if the image exists according to its absoluteAddress, return this image
-        for (ImageFile checkImage: imageList) {
+        for (ImageFile checkImage: imageFileList) {
             if (checkImage.getAbsoluteAddress().equals(absoluteAddress)) {
                 return checkImage;
             }
         }
         // if not, return a new ImageFile
         ImageFile newImageFile = new ImageFile(name, absoluteAddress);
-        imageList.add(newImageFile);
+        imageFileList.add(newImageFile);
         return newImageFile;
     }
 }

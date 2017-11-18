bbc33df2aa1ee549730a7faa0fbfd58b15fc6dba
Chenyue Wang <serenachenyue.wang@mail.utoronto.ca>
2017-11-18 01:07:40 -0500

    Created a constructor in class ImageFileManager

--- phase1/src/model/ImageFileManager.java
+++ phase1/src/model/ImageFileManager.java
@@ -4,6 +4,8 @@ package model;
 ////import java.io.IOException;
 //import java.io.File;
 //import java.io.IOException;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 //import java.util.HashMap;
 //import java.util.logging.Level;
@@ -11,23 +13,23 @@ import java.util.ArrayList;
 public class ImageFileManager {
     private static ArrayList<ImageFile> imageFileList = new ArrayList<ImageFile>();
 
-//    /**
-//     * Creates a new empty StudentManager.
-//     * @throws IOException
-//     * @throws ClassNotFoundException
-//     */
-//    public StudentManager(String filePath) throws ClassNotFoundException, IOException {
-//        students = new HashMap<String, Student>();
-//
-//        // Reads serializable objects from file.
-//        // Populates the record list using stored data, if it exists.
-//        File file = new File(filePath);
-//        if (file.exists()) {
-//            readFromFile(filePath);
-//        } else {
-//            file.createNewFile();
-//        }
-//    }
+    /**
+     * Creates a new empty StudentManager.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public ImageFileManager(String filePath) throws ClassNotFoundException, IOException {
+        imageFileList = new ArrayList<ImageFile>();
+
+        // Reads serializable objects from file.
+        // Populates the record list using stored data, if it exists.
+        File file = new File(filePath);
+        if (file.exists()) {
+            readFromFile(filePath);
+        } else {
+            file.createNewFile();
+        }
+    }
 
 
 }

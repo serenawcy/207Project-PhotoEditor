88ffa10626024823672826cff67593e060d9f798
Jiayao Lin <jiayao.lin@mail.utoronto.ca>
2017-12-01 06:13:13 +0800

    Fix show current directory folder

--- phase2/src/view_control/ManipulationManagerScene.java
+++ phase2/src/view_control/ManipulationManagerScene.java
@@ -510,10 +510,12 @@ public class ManipulationManagerScene extends Application {
         getDirectory.setOnAction(
                 (ActionEvent event) -> {
                     if (inputFile != null) {
+                      String OS = System.getProperty("os.name").toLowerCase();
+                      Boolean isLinux = OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0;
                         String path = inputFile.getFile().getParent();
                         Runtime runtime = Runtime.getRuntime();
 
-                        if (isUnix()) {
+                        if (isLinux) {
                             try {
                                 runtime.exec("nautilus " + path);
                             } catch (IOException e) {

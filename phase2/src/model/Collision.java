9833b5eb0db6b4a370c85642f1c614260f737787
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2017-11-30 15:03:58 -0500

    Added the JavaDoc for the Collision class

--- phase2/src/model/Collision.java
+++ phase2/src/model/Collision.java
@@ -1,16 +1,19 @@
 package model;
 
-import java.util.ArrayList;
 
 /**
- * This class deals with the potential file collision
+ * This class deals with the potential file collision.
+ *
+ * @author Yuhan Shao
+ * @version J.R.E 1.8.0
  */
 public class Collision {
 
     /**
+     * Returns a String of name which has added the tags to the pass-in name.
      *
      * @param nameBeforeChange the String of name before the changes
-     * @param tagsToAdd the ArrayList of String of tags which wanted to be added
+     * @param tagsToAdd        the ArrayList of String of tags which wanted to be added
      * @return the String of the current name
      */
     public String changeNameAdd(String nameBeforeChange, String tagsToAdd) {
@@ -19,7 +22,7 @@ public class Collision {
         if (tagsToAdd.contains(",")) {
             String[] tagsAdd = tagsToAdd.split(",");
 
-            for (String tags: tagsAdd) {
+            for (String tags : tagsAdd) {
                 currentName.append(" @").append(tags.trim());
             }
         } else {
@@ -28,14 +31,27 @@ public class Collision {
         return currentName.toString();
     }
 
+    /**
+     * Returns a String of name which has deleted the pass-in tag in the pass-in name.
+     *
+     * @param nameBeforeChange the String of name before the changes
+     * @param tagToDelete      the String of a tag which wanted to be deleted
+     * @return the String of the current name
+     */
     public String changeNameDelete(String nameBeforeChange, String tagToDelete) {
         return nameBeforeChange.replace(" @" + tagToDelete, "");
     }
 
+    /**
+     * Return a name which wanted to be selected.
+     *
+     * @param name      the String of a pass-in name
+     * @param getSuffix the String of a name which we want to get the suffix
+     * @return a name wanted to be selected
+     */
     public String selectName(String name, String getSuffix) {
         Integer suffixLocation = getSuffix.lastIndexOf(".");
         String suffix = getSuffix.substring(suffixLocation, getSuffix.length());
-
         return name + suffix;
     }
 }

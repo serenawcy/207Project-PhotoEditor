6a6ccaf8705b743f2d1ec6fc4529710a1be77a73
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2017-11-30 14:26:01 -0500

    Added the selectName method to the Collision class

--- phase2/src/model/Collision.java
+++ phase2/src/model/Collision.java
@@ -2,19 +2,40 @@ package model;
 
 import java.util.ArrayList;
 
+/**
+ * This class deals with the potential file collision
+ */
 public class Collision {
 
-    public String changeNameAdd(String nameBeforeChange, ArrayList<String> tagsToAdd) {
+    /**
+     *
+     * @param nameBeforeChange the String of name before the changes
+     * @param tagsToAdd the ArrayList of String of tags which wanted to be added
+     * @return the String of the current name
+     */
+    public String changeNameAdd(String nameBeforeChange, String tagsToAdd) {
         StringBuilder currentName = new StringBuilder();
-        for (String tags: tagsToAdd) {
-            currentName.append(" @").append(tags);
+
+        if (tagsToAdd.contains(",")) {
+            String[] tagsAdd = tagsToAdd.split(",");
+
+            for (String tags: tagsAdd) {
+                currentName.append(" @").append(tags.trim());
+            }
+        } else {
+            currentName.append(" @").append(tagsToAdd.trim());
         }
         return currentName.toString();
     }
 
     public String changeNameDelete(String nameBeforeChange, String tagToDelete) {
-
         return nameBeforeChange.replace(" @" + tagToDelete, "");
+    }
 
+    public String selectName(String name, String getSuffix) {
+        Integer suffixLocation = getSuffix.lastIndexOf(".");
+        String suffix = getSuffix.substring(suffixLocation, getSuffix.length());
+
+        return name + suffix;
     }
 }

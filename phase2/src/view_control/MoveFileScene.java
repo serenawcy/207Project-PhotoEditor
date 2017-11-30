613ffb5b0d0fbbc6b37ac6e442f61788d83a7309
Jiayao Lin <jiayao.lin@mail.utoronto.ca>
2017-11-30 12:28:03 +0800

    When removing a file with the same original number, fix collision

--- phase2/src/view_control/MoveFileScene.java
+++ phase2/src/view_control/MoveFileScene.java
@@ -1,10 +1,14 @@
 package view_control;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.*;
@@ -16,110 +20,120 @@ import java.io.IOException;
 
 class MoveFileScene {
 
-    /**
-     * Magic number 10
-     */
-    private static final int MAGIC10 = 10;
+  /** Magic number 10 */
+  private static final int MAGIC10 = 10;
 
-    /**
-     * Magic number 20
-     */
-    private static final int MAGIC20 = 20;
+  /** Magic number 20 */
+  private static final int MAGIC20 = 20;
 
-    /**
-     * Magic number 250
-     */
-    private static final int MAGIC250 = 250;
+  /** Magic number 250 */
+  private static final int MAGIC250 = 250;
 
-    /**
-     * Magic number 200
-     */
-    private static final int MAGIC200 = 200;
+  /** Magic number 200 */
+  private static final int MAGIC200 = 200;
 
-    /**
-     * Initialize an ImageFile
-     */
-    private static ImageFile inputFile;
+  /** Initialize an ImageFile */
+  private static ImageFile inputFile;
 
-    /**
-     * Display the Scene and construct the buttons.
-     */
-    static void display() {
-        Stage moveToStage = new Stage();
-        moveToStage.initModality(Modality.APPLICATION_MODAL);
-        moveToStage.setTitle("Select a directory");
-        moveToStage.setMinWidth(MAGIC250);
-        Label MoveInstruction = new Label();
-        MoveInstruction.setText("Please select a directory that you want the Image to move");
+  private static Stage dialogStage;
 
-        // Create two buttons
-        Button move = new Button("Select Directory");
-        Button goBack = new Button("Go back Last Page");
+  /** Display the Scene and construct the buttons. */
+  static void display() {
+    Stage moveToStage = new Stage();
+    moveToStage.initModality(Modality.APPLICATION_MODAL);
+    moveToStage.setTitle("Select a directory");
+    moveToStage.setMinWidth(MAGIC250);
+    Label MoveInstruction = new Label();
+    MoveInstruction.setText("Please select a directory that you want the Image to move");
 
-        move.setMinWidth(MAGIC200);
-        goBack.setMinWidth(MAGIC200);
+    // Create two buttons
+    Button move = new Button("Select Directory");
+    Button goBack = new Button("Go back Last Page");
 
-        // Directory Chooser adapted from ORACLE:
-        // https://docs.oracle.com/javafx/2/ui_controls/file-chooser.html
-        move.setOnAction(
-                e -> {
-                    if(inputFile != null) {
-                        String directory;
-                        DirectoryChooser directoryChooser = new DirectoryChooser();
-                        File selectedDirectory = directoryChooser.showDialog(moveToStage);
-                        if (selectedDirectory != null) {
-                            directory = selectedDirectory.getAbsolutePath();
-                            try {
-                                ImageFile inputFileSer = inputFile;
-                                ImageFile saveCurrent = inputFile;
-                                inputFileSer.changeDirectory(directory);
-                                ManipulationManagerScene.imageFileManager.delete(saveCurrent, ManipulationManagerScene.imageFileManagerPath);
-                                ManipulationManagerScene.imageFileManager.add(inputFileSer, ManipulationManagerScene.imageFileManagerPath);
-                                inputFile = inputFileSer;
-                            } catch (Exception e1) {
-                                e1.printStackTrace();
-                            }
-                            if (! isInSubDirectory(ManipulationManagerScene.getCurrentDirectory(), inputFile.getFile())) {
-                                ManipulationManagerScene.imgFiles.remove(inputFile);
-                            }
-                            ManipulationManagerScene.setImageListView(ManipulationManagerScene.imgFiles);
-                            try{
-                                ManipulationManagerScene.setFile(null);
-                                ManipulationManagerScene.setImage();
-                                ManipulationManagerScene.setPath(null);
-                            }catch (IOException e1){
-                                e1.printStackTrace();
-                            }
-                            moveToStage.close();
-                        }
-                    }
-                });
+    move.setMinWidth(MAGIC200);
+    goBack.setMinWidth(MAGIC200);
 
-        goBack.setOnAction(e -> moveToStage.close());
+    // Directory Chooser adapted from ORACLE:
+    // https://docs.oracle.com/javafx/2/ui_controls/file-chooser.html
+    move.setOnAction(
+        e -> {
+          if (inputFile != null) {
+            String directory;
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            File selectedDirectory = directoryChooser.showDialog(moveToStage);
+            if (selectedDirectory != null) {
+              directory = selectedDirectory.getAbsolutePath();
+              if (!collison(directory)) {
+                try {
+                  ImageFile inputFileSer = inputFile;
+                  ImageFile saveCurrent = inputFile;
+                  inputFileSer.changeDirectory(directory);
+                  ManipulationManagerScene.imageFileManager.delete(
+                      saveCurrent, ManipulationManagerScene.imageFileManagerPath);
+                  ManipulationManagerScene.imageFileManager.add(
+                      inputFileSer, ManipulationManagerScene.imageFileManagerPath);
+                  inputFile = inputFileSer;
+                } catch (Exception e1) {
+                  e1.printStackTrace();
+                }
+                if (!isInSubDirectory(
+                    ManipulationManagerScene.getCurrentDirectory(), inputFile.getFile())) {
+                  ManipulationManagerScene.imgFiles.remove(inputFile);
+                }
+                ManipulationManagerScene.setImageListView(ManipulationManagerScene.imgFiles);
+                  //                  ManipulationManagerScene.setFile(null);
+//                  ManipulationManagerScene.setImage();
+                  ManipulationManagerScene.setPath(inputFile);
+                  moveToStage.close();
+              }
+            }
+          }
+        });
 
-        VBox addTagLayout = new VBox(MAGIC10);
-        addTagLayout.setPadding(new Insets(MAGIC20, MAGIC20, MAGIC20, MAGIC20));
-        addTagLayout.getChildren().addAll(MoveInstruction, move, goBack);
-        addTagLayout.setAlignment(Pos.CENTER);
+    goBack.setOnAction(e -> moveToStage.close());
 
-        Scene tagScene = new Scene(addTagLayout);
-        moveToStage.setScene(tagScene);
-        moveToStage.showAndWait();
+    VBox addTagLayout = new VBox(MAGIC10);
+    addTagLayout.setPadding(new Insets(MAGIC20, MAGIC20, MAGIC20, MAGIC20));
+    addTagLayout.getChildren().addAll(MoveInstruction, move, goBack);
+    addTagLayout.setAlignment(Pos.CENTER);
+
+    Scene tagScene = new Scene(addTagLayout);
+    moveToStage.setScene(tagScene);
+    moveToStage.showAndWait();
+  }
+
+  /**
+   * Set the ImageFile
+   *
+   * @param imageFile the ImageFile
+   */
+  static void setImageFile(ImageFile imageFile) {
+    inputFile = imageFile;
+  }
+
+  private static boolean isInSubDirectory(File dir, File file) {
+
+    return file != null && (file.equals(dir) || isInSubDirectory(dir, file.getParentFile()));
+  }
+
+  private static void inappropriateMove() {
+    Alert alert = new Alert(Alert.AlertType.WARNING);
+    alert.setTitle("Inappropriate Move");
+    alert.setHeaderText("This directory already has an image with the same name!");
+    alert.setContentText("Choose another directory");
+
+    alert.showAndWait();
+  }
+
+  private static boolean collison(String directory) {
+    for (ImageFile file : ManipulationManagerScene.imgFiles) {
+      if (!inputFile.equals(file) && file.getFile().getParent().equals(directory)) {
+        if ((inputFile.getOriginalName()).equals(file.getOriginalName())) {
+          inappropriateMove();
+          return true;
+        }
+      }
     }
-
-    /**
-     * Set the ImageFile
-     *
-     * @param imageFile the ImageFile
-     */
-    static void setImageFile(ImageFile imageFile) {
-        inputFile = imageFile;
-    }
-
-
-    private static boolean isInSubDirectory(File dir, File file) {
-
-        return file != null && (file.equals(dir) || isInSubDirectory(dir, file.getParentFile()));
-
-    }
+    return false;
+  }
 }
